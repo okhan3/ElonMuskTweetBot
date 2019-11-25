510d59a34cf4edea26149d1e76600c31e7fbeefa
Omar Khan <omarkhan@umich.edu>
2019-11-24 21:17:41 -0500

    Updated generate

--- creative_ai/generate.py
+++ creative_ai/generate.py
@@ -229,16 +229,16 @@ def main():
 
 # This is how python tells if the file is being run as main
 if __name__ == '__main__':
-    # main()
+    main()
     # note that if you want to individually test functions from this file,
     # you can comment out main() and call those functions here. Just make
     # sure to call main() in your final submission of the project!
-    text = [["^::^", "^:::^", 'the', 'quick', 'brown', 'fox', "$:::$"], ["^::^", "^:::^", 'the', 'lazy', 'quick', 'dog', 'jumped', 'over', "$:::$"], ["^::^", "^:::^", 'the', 'quick', 'brown', 'dog', 'barked', "$:::$"],
-            ["^::^", "^:::^", 'dog', 'jumped', 'over', 'the', 'fox', "$:::$"], ["^::^", "^:::^", 'brown', 'cat', "$:::$"], ["^::^", "^:::^",'the','quick','brown','fox','jumped','over','the','lazy','dog', "$:::$"], ["^::^", "^:::^",'the','brown','dog','fox','quick','brown','the','dog','jumped','the','jumped',"$:::$"]]
-    x = LanguageModel()
-    x.updateTrainedData(text)
-    print(generateTokenSentence(x,1))
-    print(generateTokenSentence(x,2))
-    print(generateTokenSentence(x,3))
-    print(generateTokenSentence(x,4))
-    print(generateTokenSentence(x,5))
+    # text = [["^::^", "^:::^", 'the', 'quick', 'brown', 'fox', "$:::$"], ["^::^", "^:::^", 'the', 'lazy', 'quick', 'dog', 'jumped', 'over', "$:::$"], ["^::^", "^:::^", 'the', 'quick', 'brown', 'dog', 'barked', "$:::$"],
+    #         ["^::^", "^:::^", 'dog', 'jumped', 'over', 'the', 'fox', "$:::$"], ["^::^", "^:::^", 'brown', 'cat', "$:::$"], ["^::^", "^:::^",'the','quick','brown','fox','jumped','over','the','lazy','dog', "$:::$"], ["^::^", "^:::^",'the','brown','dog','fox','quick','brown','the','dog','jumped','the','jumped',"$:::$"]]
+    # x = LanguageModel()
+    # x.updateTrainedData(text)
+    # print(generateTokenSentence(x,1))
+    # print(generateTokenSentence(x,2))
+    # print(generateTokenSentence(x,3))
+    # print(generateTokenSentence(x,4))
+    # print(generateTokenSentence(x,5))
